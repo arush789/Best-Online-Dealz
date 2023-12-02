59ca408c241b3fb7be6a71fc703b60b60b2e573f
Karan <karandayani39@gmail.com>
2023-12-02 15:45:19 +0530

    created header and its basic styling

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 export default function Header() {
     return (
-        <h1>Header here..</h1>
+        <header>
+            <h1 className="brand-name">BODZ</h1>
+            <ul>
+                <li><NavLink>Home</NavLink></li>
+                <li><NavLink>Best Deals</NavLink></li>
+                <li><NavLink>Contact</NavLink></li>
+            </ul>
+        </header>
     )
 }
