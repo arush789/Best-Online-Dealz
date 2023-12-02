d4df630c89df1e72506afc8048131b52879a3f4b
Karan <karandayani39@gmail.com>
2023-12-02 21:07:19 +0530

    added diff pages for routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import React from "react";
-import { RouterProvider, createBrowserRouter, createRoutesFromElements, Route } from "react-router-dom"
+import { RouterProvider, createBrowserRouter, createRoutesFromElements, Route } from "react-router-dom";
+
 import Layout from "./Components/Layout";
+
+import Home from "./Pages/Home";
+import BestDeals from "./Pages/BestDeals";
+import Contact from "./Pages/Contact";
 
 const router = createBrowserRouter(createRoutesFromElements(
     <Route path="/" element={<Layout />} >
-        <Route index element={<h1>Home</h1>} />
-        <Route path="/best-deals" element={<h1>Best Deals</h1>} />
-        <Route path="/contact" element={<h1>Contact</h1>} />
+        <Route index element={<Home />} />
+        <Route path="/best-deals" element={<BestDeals />} />
+        <Route path="/contact" element={<Contact />} />
     </Route>
 ))
 
---
+++ src/Pages/BestDeals.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function BestDeals() {
+    return (
+        <h1>Best Deals page goes here...</h1>
+    )
+}
