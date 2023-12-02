7f8920cf06e4751c79f503c2c15ce0764cfeb37a
Karan <karandayani39@gmail.com>
2023-12-02 20:54:37 +0530

    made header responsive

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -2,14 +2,35 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 
 export default function Header() {
+    const [menuClass, setMenuClass] = React.useState("mob-header-menu")
+
+    function handleMenuToggle() {
+        setMenuClass(prevClass => prevClass === "mob-header-menu" ? "mob-header-menu active-menu" : "mob-header-menu")
+    }
+
     return (
         <header>
-            <h1 className="brand-name">BODZ</h1>
-            <ul>
-                <li><NavLink to="/">Home</NavLink></li>
-                <li><NavLink to="/best-deals">Best Deals</NavLink></li>
-                <li><NavLink to="/contact">Contact</NavLink></li>
-            </ul>
+            <div className="pc-header">
+                <h1 className="brand-name">BODZ</h1>
+                <ul>
+                    <li><NavLink to="/">Home</NavLink></li>
+                    <li><NavLink to="/best-deals">Best Deals</NavLink></li>
+                    <li><NavLink to="/contact">Contact</NavLink></li>
+                </ul>
+            </div>
+            <div className="mob-header">
+                <h1 className="brand-name">BODZ</h1>
+                <svg onClick={handleMenuToggle} xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" className="bi bi-list" viewBox="0 0 16 16">
+                    <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5"/>
+                </svg>
+            </div>
+            <div className={menuClass}>
+                <ul>
+                    <li><NavLink onClick={() => setMenuClass("mob-header-menu")} to="/">Home</NavLink></li>
+                    <li><NavLink onClick={() => setMenuClass("mob-header-menu")} to="/best-deals">Best Deals</NavLink></li>
+                    <li><NavLink onClick={() => setMenuClass("mob-header-menu")} to="/contact">Contact</NavLink></li>
+                </ul>
+            </div>
         </header>
     )
 }
