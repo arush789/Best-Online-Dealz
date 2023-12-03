de106e24815401c3f24e538852946eff5875c0bb
Karan <karandayani39@gmail.com>
2023-12-03 18:50:23 +0530

    disclaimer page update

--- src/Pages/Disclaimer.jsx
+++ src/Pages/Disclaimer.jsx
@@ -2,6 +2,48 @@ import React from "react";
 
 export default function Disclaimer() {
     return (
-        <h1>Disclaimer goes here...</h1>
+        <div className="disclaimer-div">
+            <h1 className="disclaimer-title">Disclaimer</h1>
+            <p>
+                In 2015, the Federal Trade Commission established disclosure guidelines 
+                to enhance transparency in online content engagement. 
+                These rules aim to inform Online Dealz.in readers about any 
+                affiliations between the blogger/publisher and external companies, 
+                ensuring awareness of financial gains tied to shared links or product promotions.
+            </p>
+            <p>
+                As per FTC guidelines, assume the following about links and posts on this site: 
+                Online Dealz.in may contain affiliate links, yielding modest compensation from specific item sales.
+            </p>
+            <p>
+                Affiliate Link Insights: Clicking these links redirects readers to the seller's site, 
+                not completing the purchase directly on Online Dealz.in. The site earns commissions 
+                from companies like Amazon, maintaining consistent prices for readers.
+            </p>
+            <p>Online Dealz engages in two main affiliate programs:</p>
+            <ul>
+                <li>
+                    <p>
+                        Amazon Affiliate Links: A participant in the Amazon Services LLC Associates Program, 
+                        allowing website owners to earn fees by linking to Amazon.in and its affiliated sites.
+                    </p>
+                </li>
+                <li>
+                    <p>
+                        Product Affiliate Links: Clicking and purchasing through these links results in Online 
+                        Dealz earning a percentage of the sale or other compensation.
+                    </p>
+                </li>
+            </ul>
+            <p>
+                Importantly, these affiliate links don't impact visitor prices, operating independently 
+                of a "pay per click" model.
+            </p>
+            <p>
+                Sponsored Content Policy: Online Dealz doesn't produce sponsored posts to uphold authenticity. 
+                Any sponsored content is transparently disclosed at a post's beginning. 
+                Product recommendations stem from personal use or endorsement for family and friends.
+            </p>
+        </div>
     )
 }
