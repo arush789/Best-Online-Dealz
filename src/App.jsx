b3a0fbaa796675b8ea7c8f8055a23406c318ff1e
Karan <karandayani39@gmail.com>
2023-12-03 17:48:54 +0530

    minor updates

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,16 @@ import Layout from "./Components/Layout";
 import Home from "./Pages/Home";
 import BestDeals from "./Pages/BestDeals";
 import Contact from "./Pages/Contact";
+import Disclaimer from "./Pages/Disclaimer";
+import About from "./Pages/About";
 
 const router = createBrowserRouter(createRoutesFromElements(
     <Route path="/" element={<Layout />} >
         <Route index element={<Home />} />
         <Route path="/best-deals" element={<BestDeals />} />
         <Route path="/contact" element={<Contact />} />
+        <Route path="/disclaimer" element={<Disclaimer />} />
+        <Route path="/about" element={<About />} />
     </Route>
 ))
 
---
+++ src/Pages/Disclaimer.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function Disclaimer() {
+    return (
+        <h1>Disclaimer goes here...</h1>
+    )
+}
