842c03875cbf0fbaca0f072147306f05e24a2da1
Dipesh Kewalramani <55015285+aruahaha@users.noreply.github.com>
2023-12-03 18:11:14 +0530

    About us page update

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -2,6 +2,37 @@ import React from "react";
 
 export default function About() {
     return (
-        <h1>About us goes here...</h1>
+        <div className="about-div">
+            <h1 className="about-title">About Us</h1>
+            <p>
+                Online Offers (Best Online Dealz) disclaims any guarantees,
+                endorsements, or warranties for external website content and products
+                linked from this Website.
+                We don't verify or endorse the accuracy or quality of information
+                and products shared here.
+            </p>
+            <p>
+                The authenticity of user-posted information and deals
+                on our website isn't verified, and inaccuracies may exist.
+                We don't make specific recommendations for the products and services presented,
+                and your decision to make a purchase is at your discretion.
+            </p>
+            <p>
+                All information is provided "as is" without any obligation to ensure accuracy.
+                Users assume all risks regarding the information's usability, quality, and suitability.
+            </p>
+            <p>
+                Users waive the right to make claims based on reliance on information from this website.
+                Your use signifies acceptance of these terms, and you cannot claim ignorance.
+            </p>
+            <p>
+                The website's development is ongoing, and we are not responsible for user access issues.
+                Information may not always be up-to-date.
+            </p>
+            <p>
+                The website is not intended for users in jurisdictions with differing laws than those
+                mandated by the laws of India, and links to external websites don't imply our endorsement of their content or updates.
+            </p>
+        </div>
     )
 }
