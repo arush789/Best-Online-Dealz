25d6e4c34a87a2b36a7b3635eef33373652947b1
Karan <karandayani39@gmail.com>
2023-12-23 16:08:49 +0530

    offer-details-page update

--- src/Pages/OfferDetail.jsx
+++ src/Pages/OfferDetail.jsx
@@ -20,11 +20,11 @@ export default function OfferDetail() {
                         <div className="offer-info">
                             <h1>{offer.title}</h1>
                             <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-                            <div>
-                                <p>₹{offer.ogPrice}</p>
-                                <p>₹{offer.offerPrice}</p>
+                            <div className="offer-price-div">
+                                <p className="og-price">₹{offer.ogPrice}</p>
+                                <p className="offer-price">₹{offer.offerPrice}</p>
                             </div>
-                            <button>Buy Now</button>
+                            <a className="buy-now-btn" href={offer.imageUrl} target="_blank">Buy Now</a>
                         </div>
                     </div>
                 )}
