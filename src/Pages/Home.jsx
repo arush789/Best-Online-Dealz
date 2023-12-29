52ef8c1d4c61bbd75ddbc79aae212cc5ae8e6bd9
Arush <55015285+aruahaha@users.noreply.github.com>
2023-12-29 20:31:39 +0530

    amazon api test

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -4,7 +4,7 @@ import { getAllOffers } from "../Server/api";
 import OfferItems from "../Components/OfferItems";
 import myImage from "/assets/images/home-img/home-img-3.png"
 import axios from "axios";
-
+const amazonPaapi = require('amazon-paapi');
 
 export function loader() {
     return defer({ offers: getAllOffers() })
@@ -16,13 +16,43 @@ export default function Home() {
 
     useEffect(() => {
 
-        axios.post(
-            '/api/amazon-api'
-        ).then(res => {
-            console.log(res.data)
-        }).catch(err => {
-            console.log(err)
-        })
+        // axios.post(
+        //     '/api/amazon-api'
+        // ).then(res => {
+        //     console.log(res.data)
+        // }).catch(err => {
+        //     console.log(err)
+        // })
+        const commonParameters = {
+            AccessKey: 'AKIAI46JMULHWNX6X66A',
+            SecretKey: 'QcOsExfSBxnFSSBvkD7+fBOiQPOT6727as35Eu4r',
+            PartnerTag: 'catchkaps0e-21', // yourtag-20
+            PartnerType: 'Associates', // Default value is Associates.
+            Marketplace: 'www.amazon.in', // Default value is US. Note: Host and Region are predetermined based on the marketplace value. There is no need for you to add Host and Region as soon as you specify the correct Marketplace value. If your region is not US or .com, please make sure you add the correct Marketplace value.
+          };
+          const requestParameters = {
+            ItemIds: ['B00X4WHP5E', 'B00ZV9RDKK'],
+            ItemIdType: 'ASIN',
+            Condition: 'New',
+            Resources: [
+              'Images.Primary.Medium',
+              'ItemInfo.Title',
+              'Offers.Listings.Price',
+            ],
+          };
+          
+          /** Promise */
+          amazonPaapi
+            .GetItems(commonParameters, requestParameters)
+            .then((data) => {
+              // do something with the success response.
+              console.log(data);
+            })
+            .catch((error) => {
+              // catch an error.
+              console.log(error);
+            });
+
     }, [])
 
     return (
--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { nodeExternalsPlugin } from 'esbuild-node-externals'
+import ViteCommonJS from 'vite-plugin-commonjs';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), nodeExternalsPlugin(),ViteCommonJS()],
   optimizeDeps: {
     exclude: ['js-big-decimal']
+  },
+  define: {
+    'process.env': process.env
   }
 })
