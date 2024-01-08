ab06ffb9df34c0823e6e5ea3f9e6c796e3e73550
Arush <55015285+aruahaha@users.noreply.github.com>
2024-01-08 15:22:31 +0530

    chat bot added

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -4,10 +4,16 @@ export default function Contact() {
     return (
         <div className="contact-div">
             <h1 className="contact-title">Contact Us</h1>
-            <p>
-                Should you have any questions about the site, advertising, or any other concerns, 
+            <p className="contact-para">
+                Should you have any questions about the site, advertising, or any other concerns,
                 feel free to get in touch with us at SUPPORT@BESTONLINEDEALZ.COM
             </p>
+            <iframe
+                src="https://www.chatbase.co/chatbot-iframe/XTytznOBwCjf5q3A9EUEr"
+                width="100%"
+                style={{height: 100  , minHeight: 700 }}
+                frameborder="0"
+            ></iframe>
         </div>
     )
 }
