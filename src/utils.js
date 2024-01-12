967f78801e93dbd3915b603c3f13cb22e54ef228
Karan <karandayani39@gmail.com>
2024-01-12 10:32:04 +0530

    Reverse 2 commits

--- src/utils.js
+++ src/utils.js
@@ -11,20 +11,12 @@ export async function requireAuth(request) {
 }
 export async function teleMessage(name,link,additional){
     const url = `https://api.telegram.org/bot${process.env.VITE_BOT_FATHER_TOKEN}/sendMessage?chat_id=${process.env.VITE_TELE_GROUP_ID}&text=${name}%0A%0A${link}%0A%0A${additional}`
-    await fetch(url, {
-        headers: {
-            "access-control-allow-origin": "*"
-        }
-    })
+    await fetch(url)
 } 
 
 export async function shortUrl(name,asin,additional){
     const url = `https://cutt.ly/api/api.php?key=${process.env.VITE_CUTLY_KEY}&short=https://best-online-dealz.vercel.app/offers/${asin}`
-    const res = await fetch(url, {
-        headers: {
-            "access-control-allow-origin": "*"
-        }
-    })
+    const res = await fetch(url)
     const data = await res.json()
     const link = data.url.shortLink
     teleMessage(name,link,additional)
