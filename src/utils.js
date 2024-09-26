023cc523cb5a1dff51138599a4600eaf820ab467
Arush <55015285+aruahaha@users.noreply.github.com>
2024-09-26 21:55:13 +0530

    short url close

--- src/utils.js
+++ src/utils.js
@@ -2,38 +2,39 @@ import axios from "axios";
 import { redirect } from "react-router-dom";
 
 export async function requireAuth(request) {
-    const isLoggedIn = localStorage.getItem("loggedIn");
+  const isLoggedIn = localStorage.getItem("loggedIn");
 
-    if (!isLoggedIn) {
-        const response = redirect(`/login`)
-        throw response
-    }
-    return null
+  if (!isLoggedIn) {
+    const response = redirect(`/login`);
+    throw response;
+  }
+  return null;
 }
 export async function teleMessage(name, link, additional) {
-    const url = `https://api.telegram.org/bot${process.env.VITE_BOT_FATHER_TOKEN}/sendMessage?chat_id=${process.env.VITE_TELE_GROUP_ID}&text=${name}%0A%0A${link}%0A%0A${additional}`
-    await fetch(url)
+  const url = `https://api.telegram.org/bot${process.env.VITE_BOT_FATHER_TOKEN}/sendMessage?chat_id=${process.env.VITE_TELE_GROUP_ID}&text=${name}%0A%0A${link}%0A%0A${additional}`;
+  await fetch(url);
 }
 
 export async function shortUrl(name, asin, additional) {
-    const url = `https://bodz-server.vercel.app/api/shortUrl/${asin}`;
+  // const url = `https://bodz-server.vercel.app/api/shortUrl/${asin}`;
 
-    try {
-        const response = await axios.get(url, {
-            headers: {
-                'Content-Type': 'application/json',
-            },
-        });
+  try {
+    //   const response = await axios.get(url, {
+    //       headers: {
+    //           'Content-Type': 'application/json',
+    //       },
+    //   });
 
-        const data = response.data;
-        const link = data.shortLink; // Adjust the property name based on the response
-        console.log(link);
-        teleMessage(name, link, additional);
-    } catch (error) {
-        console.error('Error fetching short URL:', error);
-    }
+    //   const data = response.data;
+    //   const link = data.shortLink;
+    //   console.log(link);
+    //   teleMessage(name, link, additional);
+    teleMessage(
+      name,
+      `https://best-online-dealz.vercel.app/offers/${asin}`,
+      additional
+    );
+  } catch (error) {
+    console.error("Error fetching short URL:", error);
+  }
 }
-
-
-
-
